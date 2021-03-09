668a5a8d6dc40ee5fef9d80359fe4d5b929ede7e
RTurney <r.turney93@gmail.com>
2021-03-09 16:47:01 +0000

    refactor statement_spec. All tests green

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,44 +4,47 @@ require 'statement'
 
 describe Statement do
   before(:each) do
-    @statement = Statement.new
     @date = Time.new.strftime('%d/%m/%Y')
   end
 
   describe 'on initialisation' do
     it 'will have an empty array' do
-      expect(@statement.transaction_history).to eq []
+      expect(subject.transaction_history).to eq []
     end
   end
 
   describe '.add_credit' do
     it 'will add the date, credit amount and balance to the transaction_history' do
-      @statement.add_credit(100, 0)
-      expect(@statement.transaction_history[0]).to eq([@date, '100.00', '', '100.00'])
+      subject.add_credit(100, 0)
+
+      expect(subject.transaction_history[0]).to eq([@date, '100.00', '', '100.00'])
     end
   end
 
   describe '.withdraw_debit' do
     it 'will add the date, credit amount and balance to the transaction_history' do
-      @statement.withdraw_debit(10, 20)
-      expect(@statement.transaction_history[0]).to eq([@date, '', '10.00', '10.00'])
+      subject.withdraw_debit(10, 20)
+
+      expect(subject.transaction_history[0]).to eq([@date, '', '10.00', '10.00'])
     end
   end
 
   describe '.transaction_summary' do
     it 'will return "No transactions to show" if transaction_history is empty' do
-      expect(@statement.transaction_summary).to eq 'No transactions to show'
+      expect(subject.transaction_summary).to eq 'No transactions to show'
     end
 
     it 'will return a statement in a table format' do
-      @statement.add_credit(100, 0)
-      expect(@statement.transaction_summary).to eq "date || credit || debit || balance\n#{@date} || 100.00 ||  || 100.00"
+      subject.add_credit(100, 0)
+
+      expect(subject.transaction_summary).to eq "date || credit || debit || balance\n#{@date} || 100.00 ||  || 100.00"
     end
 
     it 'returns all transactions in the correct format' do
-      @statement.add_credit(100, 0)
-      @statement.withdraw_debit(20, 100)
-      expect(@statement.transaction_summary).to eq "date || credit || debit || balance\n#{@date} ||  || 20.00 || 80.00\n#{@date} || 100.00 ||  || 100.00"
+      subject.add_credit(100, 0)
+      subject.withdraw_debit(20, 100)
+
+      expect(subject.transaction_summary).to eq "date || credit || debit || balance\n#{@date} ||  || 20.00 || 80.00\n#{@date} || 100.00 ||  || 100.00"
     end
   end
 end
